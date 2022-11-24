d63d13f954f177204bc6a7f2b83dd877512173e5
mrajski <mrajski@student.42heilbronn.de>
2022-11-24 11:59:48 +0100

    1-6 C exercises

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrajski <mrajski@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 08:20:43 by mrajski           #+#    #+#             */
+/*   Updated: 2022/11/24 11:45:11 by mrajski          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdbool.h>
+
+void	ft_putchar (char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_write_comb(int a, int b, bool last)
+{
+	ft_putchar(48 + a / 10);
+	ft_putchar(48 + a % 10);
+	ft_putchar(' ');
+	ft_putchar(48 + b / 10);
+	ft_putchar(48 + b % 10);
+	if (last)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+	bool last;
+
+	a = 0;
+	while(a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			last = !(a == 98 && b == 99);
+			ft_write_comb(a, b, last);
+			b++;
+		}
+		a++;
+	}
+}
+
+	int main ()
+{
+	ft_print_comb2();
+} 
